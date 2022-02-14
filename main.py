5e59a4aebe20ddc448c1bfe4b525003385bc2932
tahirbuyukhelvacigil <67149717+tahirbuyukhelvacigil@users.noreply.github.com>
2022-02-14 11:47:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from pydub import AudioSegment
 import pickle
 from  sklearn.preprocessing import StandardScaler,MinMaxScaler
 st.title("Akciğer Seslerindeki Anomali Tespit Uygulaması")
-st.header("Bu Web Uygulaması İle Akciğer Sesleri Sınıflandırılabilmektedir.")
-st.write("Daha Doğru Sonuçlar Elde Etmek İçin Yükleyeceğiniz Ses Dosyasının Aşağıdaki Ses Dosyası Gibi Olmasına Dikkat Ediniz.")
+st.text("Daha Doğru Sonuçlar Elde Etmek İçin Yükleyeceğiniz Ses Dosyasının Aşağıdaki Ses Dosyası Gibi Olmasına Dikkat Ediniz.")
 audio_file = open(r'sagliki-derin-nefes (13).wav', 'rb')
 audio_bytes = audio_file.read()
 st.audio(audio_bytes, format='audio/wav')
