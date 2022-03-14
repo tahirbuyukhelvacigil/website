369fb862c7533510c28520662b45910f5b2dfdbd
tahirbuyukhelvacigil <67149717+tahirbuyukhelvacigil@users.noreply.github.com>
2022-03-14 03:31:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pydub import AudioSegment
 import pickle
 from  sklearn.preprocessing import StandardScaler,MinMaxScaler
 st.title("Akciğer Seslerindeki Anomali Tespit Uygulaması")
-st.markdown("Daha Doğru Sonuçlar Elde Etmek İçin Yükleyeceğiniz Ses Dosyasının Aşağıdaki Ses Dosyası Gibi Olmasına Dikkat Ediniz.")
+st.markdown("Daha Doğru Sonuçlar :white_check_mark: Elde Etmek İçin Yükleyeceğiniz Ses Dosyasının :sound: Aşağıdaki Ses Dosyası Gibi Olmasına Dikkat Ediniz.")
 audio_file = open(r'sagliki-derin-nefes (13).wav', 'rb')
 audio_bytes = audio_file.read()
 st.audio(audio_bytes, format='audio/wav')
@@ -15,7 +15,7 @@ st.write(" ")
 st.write(" ")
 st.markdown("Sınıflandırma İşleminin Gerçekleşebilmesi İçin Aşağıdaki Bölüme Wav  Formatındaki Ses Dosyasını Yükleyiniz.Ses Dosyasının **3-7 Saniye** Arasında Olması Önerilmektedir.")
 file = st.file_uploader("",type=["mp3"])
-model = pickle.load(open("akciger_ses4.sav", 'rb'))
+model = pickle.load(open(r"akciger_ses4.sav", 'rb'))
 
 def convert_mp3_to_wav(music_file):
  sound = AudioSegment.from_mp3(music_file)
@@ -27,7 +27,6 @@ def extract(file, t1, t2):
  wav = AudioSegment.from_wav(file)
  wav = wav[1000 * t1:1000 * t2]
  wav.export('islenmis2.wav', format='wav')
-
 
 def siniflama(file):
  file_path = file
@@ -47,23 +46,21 @@ def siniflama(file):
  scaler = StandardScaler()
  scaler.fit(data)
  data = scaler.transform(data)
- st.title("Sınıflandırma Sonucu")
+ st.title("Sınıflandırma Sonucu :white_check_mark:")
  data = data.reshape(1, 80)
  output = model.predict(data)
  print(output)
  if output==[0.]:
   st.write("Gayet Sağlıklı Görünüyorsunuz :)")
   data = data.reshape(80, 1)
-  st.title('Ses Dosyasının Vektörleri')
-  data
-  st.title('Sağlıklı Günler Dileriz!')
+  st.title('Sağlıklı Günler Dileriz! :scream: ')
  else:
   st.write("Hastalıklı Bir Ses :)")
   st.title('Ses Dosyasının Vektörleri')
   data = data.reshape(80, 1)
   data
-  st.title('Sağlıklı Günler Dileriz!')
-
+  st.write("Hastalıklı Bir Ses :hospital: ")
+  st.title('Sağlıklı Günler Dileriz!:scream:')
 
 if file is None:
  st.subheader('Lütfen Sınıflandırılacak Dosyayı Yükleyiniz.')
